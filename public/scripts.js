ac135fc8fa6a1006b2adac31f3c482e0d2d75c7f
Evin Grost <evin.grost@gmail.com>
2017-02-17 14:16:55 -0600

    Getting information parsed and able to be printed.

--- public/scripts.js
+++ public/scripts.js
@@ -5,10 +5,17 @@ window.addEventListener('load', function(){
 		var infoRequest = new XMLHttpRequest();
 		infoRequest.open('GET', 'http://www.filltext.com/?rows=10&fname=%7BfirstName%7D&lname=%7BlastName%7D&tel=%7Bphone%7Cformat%7D&address=%7BstreetAddress%7D&city=%7Bcity%7D&state=%7BusState%7Cabbr%7D&zip=%7Bzip%7D&pretty=true');
 		infoRequest.onload = function(e){
-			console.log(e.target.responseText);
+			var jsonInfo = JSON.parse(infoRequest.responseText);
+			printResponse(jsonInfo);
 		}
 		infoRequest.send();
 	}
+
+	function printResponse(info){
+		console.log(info);
+		var bodyHTML = document.body;
+		bodyHTML.innerHTML = info;
+		}
 
 	getJSONInfo();
 
